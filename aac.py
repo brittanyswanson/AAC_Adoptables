e44a2c0c04176eb60c3cd378569059b515aa10b7
Brittany Swanson <brittanyjswanson@gmail.com>
2019-02-10 12:32:37 -0600

    Refactored get_sex and get_age functions.

--- aac.py
+++ aac.py
@@ -2,255 +2,276 @@
 Created by: Brittany Swanson
 Contact info: brittanyjswanson@gmail.com
 
-Program to scrape the Austin Animal Center's page for adoptable animals and add them to a database.
+Program to scrape the Austin Animal Center's page for adoptable animals and add them to a MySQL database.
 
-Last updated: 2/3/2019
+Last updated: 2/8/2019
 Created: 2/1/2019
 
 '''
+
+
 import requests
 import re
 from time import time as time, sleep as sleep
 from bs4 import BeautifulSoup
 
-
-class AACScraper:
-
-    def __init__(self):
-        self._animal_dict = dict()
+_animal_dict = dict()
 
 
-    def scrape_for_dogs(self, animal_type, page_index):
-        page = self.scrape_page(animal_type, page_index)
-        num_pages = self.process_page(page)
+#used in scrape_all_pages()
+def get_page_count(page):
+	#Create BeautifulSoup object
+	soup = BeautifulSoup(page.text, 'html.parser')
 
-        if 0 < page_index < num_pages:
-            sleep(2)
-            self.scrape_for_dogs(animal_type, page_index + 1)
+	center_tags_list = soup.findAll('center')
+	page_str = center_tags_list[1].get_text()
 
-
-    def scrape_page(self, animal_type, page_index):
-        base_url = 'http://petharbor.com/results.asp?searchtype=ADOPT&view=sysadm.v_austin&shelterlist=%27ASTN%27&where='
-        # WHERE parameter
-        if animal_type == "dog":
-            where_parameters = "type_DOG"
-        elif animal_type == "cat":
-            where_parameters = "type_CAT"
-        else:
-            where_parameters = "type_OTHER"
-
-        page_parameter = '&PAGE=' + str(page_index)
-
-        headers = {
-            'Host': 'petharbor.com',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/71.0.3578.98 Chrome/71.0.3578.98 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Referer': 'http://www.austintexas.gov/adoptablepets',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US,en;q=0.9'
-        }
-
-        page_URL = base_url + where_parameters + page_parameter
-        #print(f"Page URL is: {page_URL}")
-
-        page = requests.get(page_URL, headers=headers)
-        print(f"Scraped page {page_index}")
-        return page
+	num_pages = re.search(r'Page \d+ of (\d+)',page_str)
 
 
-    def process_page(self, page):
-        # Create BeautifulSoup object
-        soup = BeautifulSoup(page.text, 'html.parser')
+	if not num_pages:
+		return 0
 
-        # Get total pages
-        num_pages = self.get_page_count(soup)
-
-        #Get <td> tags
-        items = soup.findAll("td", {"class": re.compile("TableContent*")})
-
-        # Grab next 6 rows and add to dictionary
-        count = 0
-        while (len(items) - count) / 6 >= 1:
-            dog_id = items[count+1].get_text()
-            description = items[count+2].get_text()
-            # age find_age(description)
-            # sex
-            # fixed
-            # name
-            breed = items[count+3].get_text()
-            location = items[count+4].get_text()
-            days_in_shelter = items[count+5].get_text()
-
-            self.store_animal_in_dict(dog_id, description, breed, location, days_in_shelter)
-            count += 6
-
-        return num_pages
+	return int(num_pages.group(1))
 
 
-    '''
-    get_page_count
 
-    Finds the <center> tags in the input parameter and pulls out the string in the second <center> tag.
-    String follows the format of: "Page ? of ?"
+#used in find_sex() and find_age()
+def split_by_sentences(description):
+	#split string by sentences
+	if description.count('.') > 1:
+		sentences = description.split(".")
+	else:
+		sentences = description
 
-    Inputs:
-        BeautifulSoup object
+	#remove empty items in list
+	sentences = list(filter(None, sentences))
 
-    Return: 
-        int
+	#Loop through each item in the list and remove trailing and leading white space
+	count = 0
+	while count < len(sentences):
+		sentences[count] = sentences[count].lstrip()
+		sentences[count] = sentences[count].rstrip()
+		count+=1
 
-    '''
-    def get_page_count(self, soup):
-        center_tags_list = soup.findAll('center')
-        page_str = center_tags_list[1].get_text()
-        print(page_str)
-
-        num_pages = re.search(r'Page \d+ of (\d+)', page_str)
-
-        #print(f"Number of total pages is: {int(num_pages.group(1))}")
-
-        if not num_pages:
-            return 0
-
-        return int(num_pages.group(1))
+	return sentences
 
 
-    '''
-    store_animal_in_dict
-
-    Takes 5 inputs and adds them to a dictionary using the first input as the key and the combination of all as the value (in list form).  Adds
-    these values to the instance variable self._animal_dict[]
-
-    Inputs:
-        animal_id
-        description
-        breed
-        location
-        days_in_shelter
-
-    Return: 
-        None
-
-    '''
-    def store_animal_in_dict(self, animal_id, description, breed, location, days_in_shelter):
-        templist = []
-
-        templist.append(animal_id)
-        templist.append(description)
-        templist.append(breed)
-        templist.append(location)
-        templist.append(days_in_shelter)
-        self._animal_dict[animal_id] = templist
+#used in find_sex() and find_age()
+def narrow_to_specific_sentence(sentences,sentence_identifier):
+	for line in sentences:
+		if (line.find(sentence_identifier) != -1):
+			return line
 
 
-    def show_animals(self):
-        for animal_id, animal in self._animal_dict.items():
-            print(animal_id, animal)
+	return -1
 
 
-    '''
-    begin_menu
 
-    Draws the options on the console.
+#used in find_age()
+def age_in_days(num_str, matched_keyword, age_keywords):
+	#convert num_str to number
+	if num_str.isdigit():
+		age_number = int(num_str)
+	else:
+		return "Error converting string to int in age_in_days() function."
 
-    Inputs
-        None
+	age_days = age_number * age_keywords[matched_keyword]
 
-    Returns:
-        None
-
-    '''
-    def begin_menu(self):
-        print(30 * "-" + " MENU " + 30 * "-")
-        print("1. Scrape AAC Dogs")
-        print("2  View Dogs")
-        print("3  Run Tests")
-        print("10  Exit")
-        print(67 * "-")
+	return age_days
 
 
-    '''
-            TESTING FUNCTIONS
-    '''
-    def tests_main(self):
-        scraper = AACScraper()
-        scraper.begin_menu()
-        user_response = input("Did the menu display on the page correctly? Y or N")
-    
-        if user_response == "N":
-            print("FAILED - begin_menu()")
-        else:
-            print("PASSED - begin_menu()")
-    
-        scraper.find_age("My name is Lola.I am a white and tricolor . I am about 2 years old.")
+
+#used in process_page()
+def find_age(description):
+	animal_age = 0
+	sentences = split_by_sentences(description)
+	age_line = narrow_to_specific_sentence(sentences, "old")
+	words_in_age_sentence = age_line.split()
+
+	age_keywords = {
+				"years": 365,
+				"year": 365, 
+				"months": 30, 
+				"month": 30, 
+				"weeks": 7, 
+				"week": 7, 
+				"days": 1, 
+				"day": 1
+				}
+
+	for word in age_keywords:
+
+		if word in words_in_age_sentence:
+			age_keyword_location = words_in_age_sentence.index(word)
+		else:
+			age_keyword_location = -1
 
 
-    def find_age(self, description):
-        sentences = description.split(".")
-        sentences = list(filter(None, sentences))
+		if (age_keyword_location != -1):
+			# age_keyword was identified.  Grab previous word.
+			animal_age += age_in_days(words_in_age_sentence[age_keyword_location-1], word, age_keywords)
 
-        count = 0
-        while count < len(sentences):
-            sentences[count] = sentences[count].lstrip()
-            sentences[count] = sentences[count].rstrip()
-            count += 1
-        #todo: implement me
-        return -1
+	return animal_age
 
-    def find_sex(self, description):
-        #todo: implement me
-        return ""
 
-'''
-main
 
-Drives the menu selection process.
+#used in process_page()
+def find_sex(description):
+	sentences = split_by_sentences(description)
+	sex_line = narrow_to_specific_sentence(sentences, "female")
 
-Functions called:
-    scrape_for_dogs()
+	if sex_line == -1:
+		if narrow_to_specific_sentence(sentences, "male") == -1:
+			return "unknown"
+		else:
+			return "male"
+	elif sex_line.find("female") != -1:
+		return "female"
+	else:
+		return "unknown"
 
-'''
+
+#used in process_page()
+def store_animal_in_dict(animal_id, age, sex, breed, location, days_in_shelter):
+
+	templist = []
+
+	templist.append(animal_id)
+	templist.append(age)
+	templist.append(sex)
+	templist.append(breed)
+	templist.append(location)
+	templist.append(days_in_shelter)
+	_animal_dict[animal_id] = templist
+
+
+
+
+def process_page(page):
+	#Create BeautifulSoup object
+	soup = BeautifulSoup(page.text, 'html.parser')
+
+	#Get <td> tags
+	items = soup.findAll("td", {"class" : re.compile("TableContent*")})
+
+	#Grab next 6 rows(td) and add to dictionary
+	count = 0
+	while (len(items) - count) / 6 >= 1:
+		dog_id = items[count+1].get_text()
+		description = items[count+2].get_text()
+
+
+		age = find_age(description)
+		sex = find_sex(description)
+		#Need code for: altered/unaltered
+		#Need code for: color
+		#Need code for: name
+
+		breed = items[count+3].get_text()
+		location = items[count+4].get_text()
+		days_in_shelter = items[count+5].get_text()
+
+		store_animal_in_dict(dog_id, age, sex, breed, location, days_in_shelter)
+		count+=6
+
+
+
+#used by scrape_page()
+def build_url(animal_type, page_index):
+	base_url = 'http://petharbor.com/results.asp?searchtype=ADOPT&view=sysadm.v_austin&shelterlist=%27ASTN%27&where='
+
+	# WHERE parameter
+	if animal_type == "dog":
+		where_parameters = "type_DOG"
+	elif animal_type == "cat":
+		where_parameters = "type_CAT"
+	else:
+		where_parameters = "type_OTHER"
+
+	page_parameter = '&PAGE=' + str(page_index)
+
+	page_URL = base_url + where_parameters + page_parameter
+
+	return page_URL
+
+
+
+
+#used by scrape_all_pages()
+def scrape_page(animal_type, page_index):
+	page_URL = build_url(animal_type, page_index)
+
+	headers = {
+		'Host': 'petharbor.com',
+		'Connection': 'keep-alive',
+		'Upgrade-Insecure-Requests': '1',
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/71.0.3578.98 Chrome/71.0.3578.98 Safari/537.36',
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+		'Referer': 'http://www.austintexas.gov/adoptablepets',
+		'Accept-Encoding': 'gzip, deflate',
+		'Accept-Language': 'en-US,en;q=0.9'
+	}
+
+	page = requests.get(page_URL,headers=headers)
+	print(f"Scraped page {page_index}")
+	return page
+
+
+
+
+#used by main() and recursive
+def scrape_all_pages(animal_type, page_index):
+	page = scrape_page(animal_type, page_index)
+	total_num_pages = get_page_count(page)
+
+	process_page(page)
+
+	if 0 < page_index < total_num_pages:
+		sleep(2)
+		scrape_all_pages(animal_type, page_index + 1)
+
+
+
+
+#used by main()
+def show_animals():
+	for animal_id, animal in _animal_dict.items():
+		print(animal_id, animal)
+
+
+
+
+#used by main()
+def begin_menu():
+	print(30 * "-" + " MENU " + 30 * "-")
+	print("1. Scrape AAC Dogs")
+	print("2  View Dogs")
+	print("10  Exit")
+	print(67 * "-")
+
+
+
+
 def main():
-    scraper = AACScraper()
-    loop = True
-    
-    while loop:
-        scraper.begin_menu()
-        choice = input("Choose an option: ")
+	loop = True
 
-        if choice == "1":
-            print("Scraping for AAC dogs...")
-            scraper.scrape_for_dogs("dog", 34)
-        elif choice == "2":
-            print("Viewing dogs.")
-            scraper.show_animals()
-        elif choice == "3":
-            print("Running tests.")
-            scraper.tests_main()
-        elif choice == "10":
-            print("You've chosen to exit.  Good-bye.")
-            loop = False
-        else:
-            input("Wrong option selection. Press any key...")
+	while loop:
+		begin_menu()
+		choice = input("Choose an option: ")
+
+		if choice=="1":
+			scrape_all_pages("dog", 39)
+		elif choice=="2":
+			show_animals()
+		elif choice=="10":
+			print("You've chosen to exit.  Good-bye.")
+			loop=False
+		else:
+			input("Wrong option selection. Press any key...")
+
+
 
 
 if __name__ == "__main__":
-    main()
-
-
-'''
-    store_animal_in_dict("A0000001", "Description of an animal here.", "animal breed", "in foster", "001 days")
-    print(self._animal_dict)
-
-    #Mock up a soup object
-
-    get_page_count(soup)
-    
-
-    test_string = 'Page 1 of 9'
-    num_pages = get_page_count(test_string)
-
-    print(num_pages)
-    '''
+	main()
--- test_aac.py
+++ test_aac.py
@@ -2,72 +2,65 @@ from AAC_Adoptables.aac import *
 
 # find_age()
 def test_find_age_months_only():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 10 months old.'"
-	age = scraper.find_age(test_str)
+	age = find_age(test_str)
 
 	assert age == 300
 
 def test_find_age_month_only():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 10 month old.'"
-	age = scraper.find_age(test_str)
+	age = find_age(test_str)
 
 	assert age == 300
 
 
 def test_find_age_year_only():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 1 year old.'"
-	age = scraper.find_age(test_str)
+	age = find_age(test_str)
 
 	assert age == 365
 
 
 def test_find_age_years_only():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 1 years old.'"
-	age = scraper.find_age(test_str)
+	age = find_age(test_str)
 
 	assert age == 365
 
 
 def test_find_age_years_and_months():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 3 years and 5 months old.'"
-	age = scraper.find_age(test_str)
+	age = find_age(test_str)
 
 	assert age == 1245
 
 
+
 def test_find_age_years_and_month():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 3 years and 1 month old.'"
-	age = scraper.find_age(test_str)
+	age = find_age(test_str)
 
 	assert age == 1125
 
 
+
 # find_sex()
 def test_find_sex_male():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed male. I am about 3 years and 1 month old.'"
-	sex = scraper.find_sex(test_str)
+	sex = find_sex(test_str)
 
 	assert sex == "male"
 
 
 def test_find_sex_female():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed female. I am about 3 years and 1 month old.'"
-	sex = scraper.find_sex(test_str)
+	sex = find_sex(test_str)
 
 	assert sex == "female"
 
 
 def test_find_sex_unknown():
-	scraper = AACScraper()
 	test_str = "'Shelter staff named me Millie.I am a black and white spayed. I am about 3 years and 1 month old.'"
-	sex = scraper.find_sex(test_str)
+	sex = find_sex(test_str)
 
 	assert sex == "unknown"
